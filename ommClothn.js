aab001bdb4ebe8b9d2c5196853cf31d7bf8f2845
Michael Adebanjo <ommicang@gmail.com>
2022-07-24 01:04:37 +0100

    Permanent Dmode +

--- ommClothn.js
+++ ommClothn.js
@@ -12,13 +12,34 @@
       // DarkMode feature
 
       var lightBulb = document.getElementById("button");
+      var toggle = document.getElementById('toggle');
       lightBulb.onclick = ()=>{
+        
         document.body.classList.toggle("dark-mode");
+        toggle.classList.toggle('dark-mode');
+
+        if (document.body.classList.contains('dark-mode')){
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            localStorage.setItem('dark-mode','disabled');
+        }
+
         // lightBulb Icon change functionality
         //if(document.body.classList.contains("dark-mode")){
          // lightBulb.src = "./svgImages/darkModeSvg/sun.png";
         //} else {
          // lightBulb.src= "./svgImages/darkModeSvg/light-mobile-ui-basic-lamp-dark-mode-svgrepo-com.svg";
+        }
+
+        
+
+        //Attempt to Set Persistent Preference in Light || DarkMode Credits @ ma-no.org
+
+        
+
+        if (localStorage.getItem('dark-mode')=='enabled'){
+            document.body.classList.toggle('dark-mode');
+            toggle.classList.toggle('dark-mode');
         }
       
       //}
--- showroom/showroom.js
+++ showroom/showroom.js
@@ -1,18 +1,39 @@
-// DarkMode feature
+ // DarkMode feature
 
-var lightBulb = document.getElementById("button");
-lightBulb.onclick = ()=>{
-  document.body.classList.toggle("dark-mode");
-  // lightBulb Icon change functionality
-  //if(document.body.classList.contains("dark-mode")){
-   // lightBulb.src = "./svgImages/darkModeSvg/sun.png";
-  //} else {
-   // lightBulb.src= "./svgImages/darkModeSvg/light-mobile-ui-basic-lamp-dark-mode-svgrepo-com.svg";
-  }
+ var lightBulb = document.getElementById("button");
+ var toggle = document.getElementById('toggle');
+ lightBulb.onclick = ()=>{
+   
+   document.body.classList.toggle("dark-mode");
+   toggle.classList.toggle('dark-mode');
 
-//}
+   if (document.body.classList.contains('dark-mode')){
+       localStorage.setItem('dark-mode', 'enabled');
+   } else {
+       localStorage.setItem('dark-mode','disabled');
+   }
 
-// DarkMode Feature ends Here
+   // lightBulb Icon change functionality
+   //if(document.body.classList.contains("dark-mode")){
+    // lightBulb.src = "./svgImages/darkModeSvg/sun.png";
+   //} else {
+    // lightBulb.src= "./svgImages/darkModeSvg/light-mobile-ui-basic-lamp-dark-mode-svgrepo-com.svg";
+   }
+
+   
+
+   //Attempt to Set Persistent Preference in Light || DarkMode Credits @ ma-no.org
+
+   
+
+   if (localStorage.getItem('dark-mode')=='enabled'){
+       document.body.classList.toggle('dark-mode');
+       toggle.classList.toggle('dark-mode');
+   }
+ 
+ //}
+ 
+ // DarkMode Feature ends Here
 
 // Code Credits "Easy Tutorial" @YOUTUBE
 
