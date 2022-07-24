9e289c2d2251758adb1ec45f7009b4748a8d0636
Michael Adebanjo <ommicang@gmail.com>
2022-07-24 12:35:00 +0100

    ImgConv > Webp+

--- ommClothn.js
+++ ommClothn.js
@@ -7,22 +7,21 @@
     function vanish() {
         loader.classList.add("disappear");
     }
-   
        
       // DarkMode feature
 
       var lightBulb = document.getElementById("button");
-      var toggle = document.getElementById('toggle');
+      var ToggleOn = document.getElementById('toggle');
       lightBulb.onclick = ()=>{
         
         document.body.classList.toggle("dark-mode");
-        toggle.classList.toggle('dark-mode');
 
         if (document.body.classList.contains('dark-mode')){
             localStorage.setItem('dark-mode', 'enabled');
         } else {
             localStorage.setItem('dark-mode','disabled');
         }
+
 
         // lightBulb Icon change functionality
         //if(document.body.classList.contains("dark-mode")){
@@ -39,12 +38,61 @@
 
         if (localStorage.getItem('dark-mode')=='enabled'){
             document.body.classList.toggle('dark-mode');
-            toggle.classList.toggle('dark-mode');
+            ToggleOn.checked = true;
+
         }
       
       //}
       
       // DarkMode Feature ends Here
+
+
+       // BGmusic Starts Here
+var timer = setInterval(() => {
+    document.getElementById("popUp").style.display = "flex";
+    
+    var getMusic= document.getElementById("Yes");
+    var cancel=  document.getElementById("No");
+    
+    if(getMusic){
+    getMusic.onclick = ()=>{
+      musicPlay();
+      clearInterval(timer);
+      document.getElementById("popUp").style.display = "none";
+    }
+    } if(cancel) { // else if doesn't seem to work
+    cancel.onclick = ()=>{
+      
+      clearInterval(timer);
+      document.getElementById("popUp").style.display = "none";
+      }
+    }else {
+      document.getElementById("popUp").style.display = "none";
+    
+    }
+      
+    }, 3000);
+    
+    
+    
+    // PART B
+    
+    function musicPlay() {
+      document.getElementById('ID').play();
+    }
+    
+    function musicStop() {
+    var reset = document.getElementById('ID');
+    reset.pause();
+    if (reset.pause()){
+      reset.play(); // Prev issue solved reset code credits @adeneo (Stack)
+    }
+      else{
+        reset.currentTime = 0;
+      }
+    } 
+    // BGmusic Ends Here
+   
       
 
 // Main slider Effect {Credits > Web Dev @YouTube}
